a465415cf1c83caa9d0fadd24829767066f795ca
arlexy <61408372+arlexy@users.noreply.github.com>
2020-03-09 18:56:15 +0700

    Update PubPricesTest.java

--- assignment/src/test/java/nerdschool/bar/PubPricesTest.java
+++ assignment/src/test/java/nerdschool/bar/PubPricesTest.java
@@ -20,31 +20,31 @@ public class PubPricesTest {
     }
 
     @Test
-    public void oneBeerTest() {
+    public void OneBeer() {
         int actualPrice = pub.computeCost(Pub.ONE_BEER, false, 1);
         assertEquals(74, actualPrice);
     }
 
     @Test
-    public void testStudentsGetADiscountForBeer() throws Exception {
+    public void StudentsGetADiscountForBeer() throws Exception {
         int actualPrice = pub.computeCost(Pub.ONE_BEER, true, 1);
         assertEquals(67, actualPrice);
     }
 
     @Test
-    public void testCidersAreCostly() throws Exception {
+    public void CidersAreCostly() throws Exception {
         int actualPrice = pub.computeCost(Pub.ONE_CIDER, false, 1);
         assertEquals(103, actualPrice);
     }
 
     @Test
-    public void testProperCidersAreEvenMoreExpensive() throws Exception {
+    public void ProperCidersAreEvenMoreExpensive() throws Exception {
         int actualPrice = pub.computeCost(Pub.A_PROPER_CIDER, false, 1);
         assertEquals(110, actualPrice);
     }
 
     @Test
-    public void testACocktail() throws Exception {
+    public void ACocktail() throws Exception {
         int actualPrice = pub.computeCost(Pub.GT, false, 1);
         assertEquals(115, actualPrice);
     }
@@ -57,32 +57,32 @@ public class PubPricesTest {
     }
 
     @Test
-    public void testStudentsDoNotGetDiscountsForCocktails() throws Exception {
+    public void StudentsDoNotGetDiscountsForCocktails() throws Exception {
         int actualPrice = pub.computeCost(Pub.GT, true, 1);
         assertEquals(115, actualPrice);
     }
 
     @Test
-    public void testBacardiSpecial() throws Exception {
+    public void BacardiSpecial() throws Exception {
         int actualPrice = pub.computeCost(Pub.BACARDI_SPECIAL, false, 1);
         assertEquals(127, actualPrice);
     }
 
     @Test
-    public void testCanBuyAtMostTwoDrinksInOneGo() throws Exception {
+    public void CanBuyAtMostTwoDrinksInOneGo() throws Exception {
         expectedException.expect(RuntimeException.class);
         expectedException.expectMessage("Too many");
         pub.computeCost(Pub.BACARDI_SPECIAL, false, 3);
     }
 
     @Test
-    public void testStudentsGetDiscountsWhenOrderingMoreThanOneBeer() throws Exception {
+    public void StudentsGetDiscountsWhenOrderingMoreThanOneBeer() throws Exception {
         int actualPrice = pub.computeCost(Pub.ONE_BEER, true, 2);
         assertEquals(67*2, actualPrice);
     }
 
     @Test
-    public void testCanOrderMoreThanTwoBeers() throws Exception {
+    public void CanOrderMoreThanTwoBeers() throws Exception {
         pub.computeCost(Pub.ONE_BEER, false, 5);
     }
 }
